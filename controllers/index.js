0cb64e6faf44c722cbdfcbeac2b14081c4e20d21
Jennifer McFadden <jennifer.mcfadden@yale.edu>
2015-02-11 01:29:19 +0000

    add list of events to home page, add styling to home page

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,18 @@
 'use strict';
 
+var events = require('../models/events');
+
 /**
  * Controller that renders our index (home) page.
  */
 function index (request, response) {
+  
   var contextData = {
-    'title': 'MGT 656',
-    'tagline': 'You are doomed (just kidding).'
+    'title': 'Gatsby',
+    'tagline': 'Events made awesome',
+    'events': events.all,
+    
+   
   };
   response.render('index.html', contextData);
 }
